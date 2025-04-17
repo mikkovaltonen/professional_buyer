d707d319e992a598ab3c0d407b218c5b933eb3c2
Mikko Valtonen <scmbestoy@gmail.com>
2025-04-17 13:21:12 +0300

    fix: update Vercel and Vite configs for proper routing

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    open: true,
+    strictPort: true,
+    historyApiFallback: true
   },
   plugins: [react()],
   resolve: {
